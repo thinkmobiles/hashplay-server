b9ce2ac470c72e5ca1a6ac3d7f223591080a7f46
Killer57575 <Killer57575@gmail.com>
2015-06-22 15:45:55 +0300

    init

--- collections/users.js
+++
@@ -1,9 +0,0 @@
-var MODELS = require('../constants/models');
-
-module.exports = function (/*PostGre, ParentCollection*/) {
-   /* var Collection = ParentCollection.extend({
-        model: PostGre.Models[MODELS.USER]
-    });
-
-    return Collection;*/
-};
